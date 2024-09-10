592c4c3df4fd80fe5ebf470f3ea73973f1c790d0
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-09-10 11:24:38 +0530

    chore. animation gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
