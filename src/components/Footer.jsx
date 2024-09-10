1ceffb388f2b4cf39f94aeefbecad7ee197cb8c9
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-09-10 11:19:00 +0530

    feat. Footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-auto p-5 text-gray-500 text-center'>
+            <p>&copy; 2024 Aconews. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
