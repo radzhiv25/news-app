53abcf84fe6296fb9c24540b725a906cdf7e7799
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-09-10 15:48:14 +0530

    feat. Query and components update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import NewsContent from "./components/NewsContent"
+import Search from "./components/Search"
+import { useState } from "react"
 
 
 function App() {
+  const [query, setQuery] = useState("latest")
+  const handleSearchQuery = (searchQuery) => {
+    setQuery(searchQuery); // Update the query when search is triggered
+  };
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-      <Hero />
-      <NewsContent />
+      <Hero setQuery={setQuery}/>
+      <Search onSearch={handleSearchQuery} />
+      <NewsContent query={query} setQuery={setQuery}/>
       <Footer />
     </div>
   )
